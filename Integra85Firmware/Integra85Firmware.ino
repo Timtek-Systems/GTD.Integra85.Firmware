5717e847918ce48af52d9160d76b182635f44b42
Tim Long <Tim@tigranetworks.co.uk>
2017-11-01 01:54:50 +0000

    Experimental code to step the rotator motor

--- Integra85Firmware/Integra85Firmware.ino
+++ Integra85Firmware/Integra85Firmware.ino
@@ -1,15 +1,63 @@
-/*
- Name:		Integra85Firmware.ino
- Created:	10/31/2017 9:31:44 PM
- Author:	Tim
-*/
+#include "Integra85_hardware.h"
 
-// the setup function runs once when you press reset or power the board
-void setup() {
+void setup() 
+	{
+	Serial.begin(115200);   //Initialize serial for debugging and control
+	//BtSerial.begin(9600);
+	pinMode(M1_STEP_PIN, OUTPUT);
+	pinMode(M1_DIRECTION_PIN, OUTPUT);
+	pinMode(M1_ENABLE_PIN, OUTPUT);
+	digitalWrite(M1_ENABLE_PIN, HIGH);
+	pinMode(M2_STEP_PIN, OUTPUT);
+	pinMode(M2_DIRECTION_PIN, OUTPUT);
+	pinMode(M2_ENABLE_PIN, OUTPUT);
+	digitalWrite(M2_ENABLE_PIN, HIGH);
+	StopAndClearTimer1();
+	sei();
+	StartMotor2(600);
+	}
 
-}
 
-// the loop function runs over and over again until power down or reset
-void loop() {
-  
-}
+void loop() 
+	{
+	if (millis() > 10000) StopMotor2();
+	}
+
+void StopAndClearTimer1()
+	{
+	TCCR1B = 0x00;       // Stop Timer Counter 1 
+	TCNT1 = 0;           // Clear Timer Counter 1 
+	OCR1A = 0x21;
+	OCR1B = 0x21;        // 10 miliseconds 
+	TCCR1A = 0x00;       // Generate no outputs 
+	}
+
+void StartTimer1(uint16_t periodMilliseconds)
+	{
+	auto timerCounts = periodMilliseconds ;
+	OCR1A = timerCounts;
+	TIMSK1 |= (1 << OCIE1A); // Enable timer 1, OCR1A interrupt
+	TCCR1A = 0x00;			 // Generate no outputs 
+	TCCR1B = 0x09;			 // No prescale, CTC mode, compare with OCR1A
+	}
+
+void StartMotor2(uint16_t periodMilliseconds)
+	{
+	digitalWrite(M2_ENABLE_PIN, LOW);
+	StartTimer1(periodMilliseconds);
+	}
+
+void StopMotor2()
+	{
+	StopAndClearTimer1();
+	digitalWrite(M2_ENABLE_PIN, HIGH);
+	}
+
+static bool stepState = false;
+
+ISR(TIMER1_COMPA_vect)
+	{
+	stepState = !stepState;
+	digitalWrite(M2_STEP_PIN, stepState);
+	}
+
---
+++ Integra85Firmware/Integra85_hardware.h
@@ -0,0 +1,15 @@
+#pragma once
+// Analogue input channels
+#define TEMPERATURE_SENSOR_CHANNEL  (11)
+#define TOUCH_SENSOR_CHANNEL		(0)
+//Motor1 pins
+#define M1_STEP_PIN					(7)
+#define M1_DIRECTION_PIN			(6)
+#define M1_ENABLE_PIN				(13)
+//Motor2 pins
+#define M2_STEP_PIN					(5)
+#define M2_DIRECTION_PIN			(4)
+#define M2_ENABLE_PIN				(12)
+//Software serial
+#define SW_RX						(2)
+#define SW_TX						(3)
